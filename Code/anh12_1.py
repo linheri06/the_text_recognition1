91a122e7ebf57ef5c0d1cebf508136bf837c6474
linheri06 <100428580+linheri06@users.noreply.github.com>
2023-08-18 09:41:20 +0700

    Update anh12_1.py

--- Code/anh12_1.py
+++ Code/anh12_1.py
@@ -3,7 +3,7 @@ from paddleocr import PaddleOCR,draw_ocr
 # You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
 # to switch the language model in order.
 ocr = PaddleOCR(use_angle_cls=True, lang='japan') # need to run only once to download and load model into memory
-img_path = 'D:\\Downloads\\ppocr_img\\imgs_words\\japan\\1.jpg'
+img_path = 'D:\\Downloads\\scene\\SceneTrialTest\\ryoungt_05.08.2002\\aPICT0034.JPG'
 result = ocr.ocr(img_path, cls=True)
 for idx in range(len(result)):
     res = result[idx]
@@ -12,7 +12,7 @@ for idx in range(len(result)):
 
 
 # draw result
-'''from PIL import Image
+from PIL import Image
 import PIL
 print(PIL.__version__)
 result = result[0]
@@ -22,4 +22,4 @@ txts = [line[1][0] for line in result]
 scores = [line[1][1] for line in result]
 im_show = draw_ocr(image, boxes, txts, scores, font_path='D:\\Downloads\\ppocr_img\\fonts\\simfang.ttf')
 im_show = Image.fromarray(im_show)
-im_show.save('result.jpg')'''
+im_show.save('result.jpg')
